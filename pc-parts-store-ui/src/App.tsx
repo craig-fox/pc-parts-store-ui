c752d7d9be071bab974f993e436ce1e94e0592f5
Craig Fox <craig.fox@icloud.com>
2026-07-15 17:52:23 +1200

    First commit

---
+++ pc-parts-store-ui/src/App.tsx
@@ -0,0 +1,15 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+    return (
+        <>
+            <Navbar />
+
+            <main>
+                <h1>Welcome!</h1>
+            </main>
+        </>
+    );
+}
+
+export default App;
